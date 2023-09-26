a78037a174e4e3b10df1ee732dabc33b0a1a9e57
ArtemK64 <79468247+ArtemK64@users.noreply.github.com>
2023-09-26 13:02:32 +0400

    Added second part of task

--- task-1/app.js
+++ task-1/app.js
@@ -1,7 +1,7 @@
 let a = Math.floor(Math.random() * 100);
 
 console.log('First part of task');
-console.log('if-else realization: ');
+console.log('if-else realization:');
 
 if ((a > 10 ? a : a * 2) > 5) {
     console.log((2 * a) + 1);
@@ -13,7 +13,7 @@ if ((a > 10 ? a : a * 2) > 5) {
     }
 }
 
-console.log('Switch realization: ');
+console.log('Switch realization:');
 
 switch (true) {
     case (a > 10 ? a : a * 2) > 5:
@@ -25,3 +25,69 @@ switch (true) {
     default:
       console.log((a % 2 == 0 ? 6 : 7));
 }
+
+console.log('Second part of task');
+console.log('if-else realization:')
+
+function manyChecks() {
+    let a = Math.floor(Math.random() * 20) + 1;
+    console.log(`a = ${a}`);
+    
+    if (a > 10) {
+        if (a == 5) {
+            console.log('a is bigger than 10 and an example of a special case');
+        } else {
+            console.log('a is bigger than 10');
+        }
+        if (a != 15) {
+            console.log('but a is not 15');
+        }
+    } else {
+        if (a == 5) {
+            console.log('a is less than or equal to 10 and an example of a special case');
+        } else {
+            console.log('a is less than or equal to 10');
+        }
+    }
+    if (a % 2 == 0) {
+        console.log('and a is even');
+    } else {
+        console.log('and a is odd');
+    }
+}
+
+manyChecks();
+
+console.log('Switch realization:');
+function manyChecks() {
+    let a = Math.floor(Math.random() * 20) + 1;
+    console.log(`a = ${a}`);
+
+    switch (true) {
+        case a > 10: 
+            if (a === 5) {
+                console.log('a is bigger than 10 and an example of a special case');
+            } else {
+                console.log('a is bigger than 10');
+            }
+            if (a !== 15) {
+                console.log('but a is not 15');
+            }
+            console.log('and a is greater than 5');
+            break;
+        case a === 5: 
+            console.log('a is less than or equal to 10 and an example of a special case');
+            console.log('and a is less than or equal to 5');
+            break;
+        default: 
+            console.log('a is less than or equal to 10');
+            console.log('and a is less than or equal to 5');
+    }
+    if (a % 2 === 0) {
+        console.log('and a is even');
+    } else {
+        console.log('and a is odd');
+    }
+}
+
+manyChecks();
